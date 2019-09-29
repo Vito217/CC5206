b68a0a4e329824afde0fefa526509e300b056ec2
kcossio <kyrantonia@gmail.com>
2019-09-29 01:43:02 -0300

    agregar mas wordclouds

--- union.py
+++ union.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-
+import string
 presidents = ['pinera', 'bachelet', 'allende', 'macri', 'kirchner', 'fernandez']
 
 with open('data/csv/union.csv', 'w') as csvfile:
@@ -8,18 +8,19 @@ with open('data/csv/union.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     csv.field_size_limit(sys.maxsize)
-
     for president in presidents:
         with open("data/csv/"+president + '.csv') as csvfile:
             readCSV = csv.DictReader(csvfile)
             for row in readCSV:
-                print(row)
-
+                content=str(row['content']).lower()
+                print(content)
+                translator = str.maketrans('', '', string.punctuation)
+                content.translate(translator)
                 writer.writerow(
                     {'president': president,
                      'title': row['title'],
                      'date': row['date'],
-                     'content': row['content'],
-                     'size': len(row['content']),
+                     'content': content,
+                     'size': len(content.split()),
                      }
                 )
