e67aae55ab257a8c346cc9197e134e6b7684dd9b
Vito217 <vito.segurac@hotmail.com>
2019-10-01 14:44:38 -0300

    Correcciones al plot, se muestran tamanos de discurso

--- cluster.py
+++ cluster.py
@@ -18,58 +18,125 @@ from datetime import datetime
 
 markers = ["o", "v", "^", "<", ">", "s", "p", "P", "*", "h", "+", "X", "d", "1", "2", "3", "4"]
 colors = np.array(["#1c3969", "#298294", "#177548", "#177522", "#4b8a1c", "#646e0c", "#695311",
-                   "#7a3f11", "#7a1111", "#521173"])
+                   "#7a3f11", "#7a1111", "#521173", "red"])
 
 
 def load_stopwords(string):
+    """
+    Lee archivos .txt con stopwords. Deben estar separadas por salto de linea
+    :param string:
+    :return:
+    """
     print("Reading Stopwords")
+    # Si el archivo existe
     if os.path.isfile(string):
         with open(string, 'r', encoding="utf8") as words:
             sw = words.read().split('\n')
+    # Si el nombre es english o spanish
     else:
         sw = stopwords.words(string)
     return sw
 
 
-def load_data(path, subpath, nrows=None, ignore=None, filt=None):
+def load_data(path, subpath, nrows=None, ignore=None, filt=None, oversampling=False, undersampling=False):
+    """
+    Carga datos desde un path, generalmente data/text, y un subpath, generalmente raw.
+    Se puede ignorar presidentes y filtrar segun una expresion regular.
+    Returna una tupla data, labels
+    :param path:
+    :param subpath:
+    :param nrows:
+    :param ignore:
+    :param filt:
+    :return:
+    """
     print("Reading all data and labels")
+
+    # Lista que tiene tuplas (discurso, persidente)
     data_labels = []
     for subdir, dirs, files in os.walk(path):
         for file in files:
+
+            # Params es de la forma (dir, subdir, file)
             params = subdir.split("\\")
+
+            # No se lee el archivo si:
+            # * Esta en ignore
+            # * Si no esta en filter
+            # * Si tiene alguna extension
             if not(file.endswith(".py") or file.endswith(".txt")) \
                     and (len(params) == 3 and params[2] == subpath):
                 if not((ignore is not None and params[1] in ignore) or
                         (filt is not None and not filt.search(file))):
                     with open(os.path.join(subdir, file), 'r', encoding='utf8') as f:
-                        data_labels.append([f.read(), params[1]])
+                        # Se guarda la tupla (discurso, presidente)
+                        text = f.read()
+                        data_labels.append([text, params[1]])
+
+    # Permutacion al azar
     shuffle(data_labels)
+
+    # Si se especifico un numero de filas
     if nrows is not None:
         data_labels = data_labels[:nrows]
-    # data_labels = sorted(data_labels, key=lambda x: x[1])
+
+    # Separamos data y labels
     data = [corpus for corpus, _ in data_labels]
     labels = [label for _, label in data_labels]
-    return data, labels
+    if oversampling or undersampling:
+        if oversampling:
+            sampler = RandomOverSampler()
+        else:
+            sampler = RandomUnderSampler()
+        data, labels = sampler.fit_resample(np.reshape(data, (-1, 1)), np.reshape(labels, (-1, 1)))
+        data, labels = np.reshape(data, (-1, )), np.reshape(labels, (-1, ))
+    size = np.reshape([len(text) for text in data], (-1, ))
+    return data, labels, (size / np.linalg.norm(size))*1000
 
 
 def get_number_of_clusters(labels):
+    """
+    Retorna numero de clusters equivalente al numero de labels
+    :param labels:
+    :return:
+    """
     print("Indexing labels")
-    # le = LabelEncoder()
-    # target_ind = le.fit_transform(labels)
     unique = np.unique(labels)
     return len(unique)
 
 
 def vectorize(data, stop_words, ngram_min, ngram_max):
+    """
+    Calcula el TF-IDF de una matriz de textos. Incluye rango de ngramas
+    :param data:
+    :param stop_words:
+    :param ngram_min:
+    :param ngram_max:
+    :return:
+    """
     print("Getting Bag Of Words")
     vectorizer = TfidfVectorizer(stop_words=stop_words, ngram_range=(ngram_min, ngram_max))
     x = vectorizer.fit_transform(data)
     return x
 
 
-def plot_clusters(x, labels, dim, n_clusters, cluster_type="kmeans", save=False):
+def plot_clusters(x, labels, size, dim, n_clusters, cluster_type="kmeans", save=False):
+    """
+    Grafica los clusters.
+    :param x:
+    :param labels:
+    :param dim:
+    :param n_clusters:
+    :param cluster_type:
+    :param save:
+    :return:
+    """
+
+    # Traspasamos data a dos o tres dimensiones usando PCA
     trunc = TruncatedSVD(n_components=dim)
     x = trunc.fit_transform(x)
+
+    # Elegimos el tipo de clustering
     if cluster_type == "mbkmeans":
         km = MiniBatchKMeans(n_clusters=n_clusters)
     elif cluster_type == "affprop":
@@ -88,40 +155,63 @@ def plot_clusters(x, labels, dim, n_clusters, cluster_type="kmeans", save=False)
         km = Birch()
     else:
         km = KMeans(n_clusters=n_clusters)
+
+    # Se calculan los clusters. Los centroides se almacenan en km
     km.fit(x)
+    # LB son los indices del cluster al que pertenece cada fila de datos
     lb = km.labels_
+
+    # Como se hace mas de un plot a la vez, separamos la data por presidente
     data_dict = {}
     for i in range(len(x)):
+
+        # Se le asigna un marcador y se guarda una lista de sus discursos y su respectivo cluster
         label = labels[i]
         if label not in data_dict:
-            data_dict[label] = {'data': [], 'clusters': [], 'marker': markers.pop(0)}
+            data_dict[label] = {'data': [], 'clusters': [], 'sizes': [], 'marker': markers.pop(0)}
         data_dict[label]['data'].append(x[i])
         data_dict[label]['clusters'].append(lb[i])
+        data_dict[label]['sizes'].append(size[i])
+
+    # Si el grafico es en 2D
     if dim == 2:
+
+        # Iteramos y hacemos un plot por cada presidente
         for key in data_dict.keys():
             x = [row[0] for row in data_dict[key]['data']]
             y = [row[1] for row in data_dict[key]['data']]
-            c = [num for num in data_dict[key]['clusters']]
+            c = data_dict[key]['clusters']
+            s = data_dict[key]['sizes']
             marker = data_dict[key]['marker']
-            l = plt.scatter(x, y, c=colors[c], marker=marker, label=key)
+            l = plt.scatter(x, y, c=colors[c], s=s, marker=marker, label=key, edgecolors="black")
         plt.legend()
         plt.show()
+
+        # Guardamos el plot
         if save:
             if not os.path.exists("results/speech_clustering"):
                 os.makedirs("results/speech_clustering")
             plt.savefig("results/speech_clustering/{}.png".format(datetime.now().strftime("%Y%m%d-%H%M%S")))
+
+    # Si el grafico es en 3D
     elif dim == 3:
+
         plot = plt.figure()
         ax = Axes3D(plot)
+
+        # Iteramos y hacemos un plot por cada presidente
         for key in data_dict.keys():
             x = [row[0] for row in data_dict[key]['data']]
             y = [row[1] for row in data_dict[key]['data']]
             z = [row[2] for row in data_dict[key]['data']]
-            c = [num for num in data_dict[key]['clusters']]
+            c = data_dict[key]['clusters']
+            s = data_dict[key]['sizes']
             marker = data_dict[key]['marker']
-            ax.scatter(x, y, z, c=colors[c], marker=marker, label=key)
+            ax.scatter(x, y, z, c=colors[c], s=s, marker=marker, label=key, edgecolors="black")
         plot.legend()
         plot.show()
+
+        # Guardamos el plot
         if save:
             if not os.path.exists("results/speech_clustering"):
                 os.makedirs("results/speech_clustering")
@@ -132,18 +222,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate clusters")
     parser.add_argument("--oversampling", action='store_true')
     parser.add_argument("--undersampling", action='store_true')
-    parser.add_argument("-stopwords", type=str, help="Path to stopwords", required=True)
+    parser.add_argument("-stopwords", type=str, help="Path to stopwords, or english or spanish", required=True)
     parser.add_argument("-data", type=str, help="Path to dataset. Every folder name is the label.", required=True)
     parser.add_argument("-subdata", type=str, help="Subdata available: raw, anio, anio_mes", required=True)
     parser.add_argument("-ngram_min", type=int, help="Min range of n-gram", required=True)
     parser.add_argument("-ngram_max", type=int, help="Max range of n-gram", required=True)
     parser.add_argument("-dim", type=int, help="2 for 2D plot, 3 for 3D plot", required=True)
-    parser.add_argument("-type", type=int, help="kmeans, mbkmeans, affprop, mshift, spec, aggc, dbscan, optics, birch",
+    parser.add_argument("-type", type=str, help="kmeans, mbkmeans, affprop, mshift, spec, aggc, dbscan, optics, birch",
                         required=False)
     parser.add_argument("-nrows", type=int, help="Number of rows to use", required=False)
     parser.add_argument("-ignore", type=str, help="CSV list with candidates (e.g. pinera,bachelet)", required=False)
     parser.add_argument("-filter", type=str, help="Regex used to filter some texts", required=False)
-    parser.add_argument("-nclusters", type=str, help="Number of clusters to use", required=False)
+    parser.add_argument("-nclusters", type=int, help="Number of clusters to use", required=False)
     parser.add_argument("--save", action='store_true')
 
     pargs = parser.parse_args()
@@ -151,18 +241,15 @@ if __name__ == '__main__':
     ignore = pargs.ignore.split(",") if pargs.ignore else None
     nrows = pargs.nrows if pargs.nrows else None
     filt = re.compile(pargs.filter) if pargs.filter else None
-    data, labels = load_data(pargs.data, pargs.subdata, nrows, ignore, filt)
+    over = True if pargs.oversampling else False
+    under = True if pargs.undersampling else False
+    data, labels, size = load_data(pargs.data, pargs.subdata, nrows, ignore, filt,
+                                   oversampling=over, undersampling=under)
     n_clusters = pargs.nclusters if (pargs.nclusters and pargs.nclusters >= 1) else get_number_of_clusters(labels)
     cluster_type = pargs.type if pargs.type else "kmeans"
     ngram_min = pargs.ngram_min
     ngram_max = pargs.ngram_max
     x = vectorize(data, stopwords, ngram_min, ngram_max)
-    if pargs.oversampling:
-        sampler = RandomOverSampler()
-        x, labels = sampler.fit_resample(x, labels)
-    elif pargs.undersampling:
-        sampler = RandomUnderSampler()
-        x, labels = sampler.fit_resample(x, labels)
     dim = pargs.dim
     save = pargs.save
-    plot_clusters(x, labels, dim, n_clusters, cluster_type, save)
+    plot_clusters(x, labels, size, dim, n_clusters, cluster_type, save)
